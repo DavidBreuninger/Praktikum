8eaf27545fba67d2016126ad6fd7f3290072ce35
Duc Anh Pham <ducanhpham555@outlook.de>
2026-03-05 20:24:31 +0100

    Remove NAs from data

--- data_cleansing.R
+++ data_cleansing.R
@@ -3,8 +3,12 @@ library(dplyr)
 library(stringr)
 library(checkmate)
 library(lubridate)
+source("functions.R")
 
 # read csv files
 Mobilitaet <- read.csv("Data/indikat2510_bevoelkerung_mobilitaetsziffer_28_10_25.csv")
-Bevölkerungsdichte <- read.csv("Data/indikat2510_bevoelkerung_bevoelkerungsdichte_28_10_25.csv")
+Bevoelkerungsdichte <- read.csv("Data/indikat2510_bevoelkerung_bevoelkerungsdichte_28_10_25.csv")
 
+# remove columns or lines consisting of only NAs
+Mobilitaet <- remove_NAs(Mobilitaet)
+Bevoelkerungsdichte <- remove_NAs(Bevoelkerungsdichte)
