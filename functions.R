3b9a022cb45a123e347dedfb4ed9163c85089b6d
Duc Anh Pham <ducanhpham555@outlook.de>
2026-03-22 22:53:27 +0100

    added two functions for Excel sheets

--- functions.R
+++ functions.R
@@ -18,3 +18,39 @@ remove_Auspraegung <- function(df) {
     mutate_all(~ gsub("\\(auch innerhalb der räumlichen Einheit\\)","",.)) %>%
   return(df)
 }
+
+# vectors for functions read_xls_matrix and read_xlsx_matrix
+Mobilitaet_thin <- read.csv("Clean_Data/Mobilitaet_thin.csv")
+Bezirke <- Mobilitaet_thin %>%
+  select(Raumbezug) %>% 
+  distinct() %>% 
+  filter(Raumbezug != "Stadt München") %>%
+  pull()
+jahrbuch_spaltennamen <- c("Anfangsbezirk", Bezirke, "zusammen")
+jahrbuch_spaltentypen <- c("text", rep("numeric", 26))
+
+# FUNC3: read_xls_matrix, read_xls function with set parameters, for the given data
+read_xls_matrix <- function(filepath) {
+  require(readxl)
+  require(dplyr)
+  df <- read_xls(filepath, 
+                 col_names = jahrbuch_spaltennamen,
+                 skip = 4,
+                 col_types = jahrbuch_spaltentypen,
+                 n_max = 26)
+  return(df)
+}
+
+# FUNC4: read_xlsx_matrix, read_xlsx function with set parameters, for the given data
+read_xlsx_matrix <- function(filepath) {
+  require(readxl)
+  require(dplyr)
+  df <- read_xlsx(filepath, 
+                 col_names = jahrbuch_spaltennamen,
+                 skip = 4,
+                 col_types = jahrbuch_spaltentypen,
+                 n_max = 26)
+  return(df)
+}
+
+# FUNC5: matrix_to_long, pivots matrices to a long format for work with ggplot2
